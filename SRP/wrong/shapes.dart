c56749d44e461b81befd56188963d9d2076b97fe
alirezaseif20 <alirezaariye@gmail.com>
2024-06-11 12:39:05 +0330

    feat: add S (Single Responsibility)

---
+++ SRP/wrong/shapes.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Shapes {
+  // Calculations
+  double? squareArea(double l) {
+    /* ... */
+  }
+
+  double? circleArea(double r) {
+    /* ... */
+  }
+
+  double? triangleArea(double b, double h) {
+    /* ... */
+  }
+
+  // Paint to the screen
+  void paintSquare(Canvas c) {
+    /* ... */
+  }
+  void paintCircle(Canvas c) {
+    /* ... */
+  }
+  void paintTriangle(Canvas c) {
+    /* ... */
+  }
+}
--- android/app/src/main/kotlin/com/example/solid/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.solid
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
